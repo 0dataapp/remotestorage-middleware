15151809b002afa0f0b4d280f0322c18d4be0417
Rosano Coutinho <github.com@xeari.com>
2026-01-28 10:37:01 +0100

    use json parser

--- example.js
+++ example.js
@@ -3,5 +3,6 @@ const main = require('./main.js');
 
 const port = process.env.PORT || 3000;
 express()
+  .use(express.json())
   .use(main.default.handle)
   .listen(port, () => console.info(`> Running on port ` + port));
