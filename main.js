9fb802158e6c5ede0d12658e3e0638b1f355887c
Rosano Coutinho <github.com@xeari.com>
2026-02-09 09:06:55 +0100

    send body to silence firefox error on delete

--- main.js
+++ main.js
@@ -135,7 +135,6 @@ const mod = {
 			.status(200);
 
 		if (['HEAD', 'DELETE'].includes(req.method))
-			return res.end();
 
 		return isFolderRequest ? res.json({
 			'@context': 'http://remotestorage.io/spec/folder-description',
